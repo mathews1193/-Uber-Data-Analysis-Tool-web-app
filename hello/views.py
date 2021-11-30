fd19817991bfb36239430742995db3ceba9449a2
mathews1193 <mathews1193@gmail.com>
2021-11-30 15:59:54 -0600

    complete codebase (still needs work)

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,13 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from matplotlib.ticker import FuncFormatter #Call formatter function to format tick values
+from matplotlib.offsetbox import (OffsetImage, AnnotationBbox) #Create image box
+from matplotlib.patches import Ellipse #Draw ellips
+from pandas.tseries.holiday import USFederalHolidayCalendar
 import seaborn as sns
+import chart_studio.plotly as py
 import time
 import datetime
 
@@ -21,12 +27,401 @@ pd.set_option('display.float_format', lambda x: '%.4f' % x)
 #filename = 'uber_nyc_data.csv'
 #urllib.request.urlretrieve(url, filename)
 
-
 df_uber = pd.read_csv('uber_nyc_data.csv')
-infor = df_uber.info() 
 head = df_uber.head()
 
-import datetime
+allData=[]
+for i in range(head.shape[0]):
+    temp=head.iloc[i]
+    allData.append(dict(temp))
+
+def dateParser(s):
+    """
+    Function that takes a string in the format yyyy-mm-dd hh:mm:ss, and
+    returns the same as a datetime object.
+    """
+    return datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+
+#Apply parser to data
+t0 = time.time()
+df_uber['pu_date_hour'] = df_uber.pickup_datetime.apply(dateParser)
+time.time() - t0
+
+#Data encompasses exactly 365 days from Sept 1st, 2014 to Sep 1st, 2015.
+beginning = df_uber.pu_date_hour.min()
+end = df_uber.pu_date_hour.max()
+
+#Drop repeated column
+df_uber = df_uber.drop('pickup_datetime', axis=1)
+
+#if return_name is True, then name is series value and date is the series index, otherwise date is series value.
+holidays = USFederalHolidayCalendar().holidays(beginning, end, return_name = True)
+
+#Extract the weekday for each holiday
+holidays.index.map(lambda x: x.strftime('%a'))
+
+#For completeness, let's add the day before and after the holidays (better: add day after if Thursday)
+holidays_all = pd.concat([holidays, "Day After " + holidays.shift(1, 'D'), "Day Before " + holidays.shift(-1, 'D')])
+holidays_all = holidays_all.sort_index()
+holidays_all.head(10)
+
+#Create a column with the date, without the time. This will be useful later.
+t0 = time.time()
+df_uber['pu_date'] = pd.Series(map(lambda x: x.astype('datetime64[D]'), df_uber['pu_date_hour'].values))
+#df_uber['pu_date'] = df_uber['pu_date_hour'].dt.date #this is way less memory effective and 11 sec slower to execute
+time.time() - t0
+
+#Get month and year from pick up timestamp
+df_uber['year'] = df_uber['pu_date_hour'].dt.year
+df_uber['month'] = df_uber['pu_date_hour'].dt.month
+
+#Get trip pick up day of the month
+t0 = time.time()
+df_uber['day'] = df_uber['pu_date_hour'].dt.day #.apply(lambda x: x.day) = 105 seconds vs < 2 seconds with dt
+time.time() - t0
+
+#Get trip pick up day of the month
+t0 = time.time()
+df_uber['day'] = df_uber['pu_date_hour'].dt.day #.apply(lambda x: x.day) = 105 seconds vs < 2 seconds with dt
+time.time() - t0
+
+#Get trip pick up hour from timestamp
+t0 = time.time()
+df_uber['hour'] = df_uber['pu_date_hour'].dt.hour #< 2 seconds vs 101 seconds using apply lambda
+time.time() - t0
+
+head2 = df_uber.head()
+
+allData2=[]
+for i in range(head2.shape[0]):
+    temp=head2.iloc[i]
+    allData2.append(dict(temp))
+
+def duration_to_minutes(s):
+    """
+    Function that takes a string with the hh:mm:ss format and
+    returns the integer equivalent of the total time in minutes, 
+    or zero for missing values in a Pandas dataframe.
+    """
+    if pd.isnull(s):
+        val = 0 #note: this fills with 0 the 38 instances with null (missing) values
+    else:
+        hms = s.split(':')
+        val = int(hms[0])*60 + int(hms[1]) + int(hms[2])/60.0
+    return val
+
+#Transform the trip duration string into trip duration in minutes:
+t0 = time.time()
+df_uber['duration_min'] = df_uber.trip_duration.apply(duration_to_minutes) 
+time.time() - t0
+
+#Calculate average trip speed (mph) to help in understanding outliers (like trips with duration >10 h):
+df_uber['trip_mph_avg'] = df_uber.trip_distance/(df_uber.duration_min/60.0)
+
+#Drop redundant trip_duration columns
+df_uber = df_uber.drop('trip_duration', axis=1)
+df_uber = df_uber.drop('pu_date_hour', axis=1)
+
+#Source: http://uberestimate.com/prices/New-York-City/ for Uber X
+base_fare = 2.55
+per_minute = 0.35
+per_mile = 1.75
+min_fare = 8
+
+#Using eval() is even more efficient for large datasets than resorting to Numpy! 
+#Local variables must be followed by @. Only for arithmetic, cannot use if statement, for example.
+df_uber['est_revenue'] = df_uber.eval('@base_fare + duration_min * @per_minute + trip_distance * @per_mile')
+df_uber.loc[df_uber.est_revenue < 8, 'est_revenue'] = min_fare
+
+df_uber.head()
+
+#Create dataframe to be used for visualization with exactly 365 days of data, and max trip duration of 16h:
+df_viz = df_uber[(df_uber.pu_date != datetime.datetime(2015, 9, 1)) & (df_uber.duration_min <= 960)].copy() #1975 cases
+
+#Descriptive statistics for numerical features:
+stats = df_viz[['duration_min', 'trip_distance', 'trip_mph_avg', 'est_revenue']].describe()
+
+#Create functions to format tick numbers
+def thousands_comma(x, pos):
+    """
+    Args are the value and tick position. 
+    Returns number with thousands comma and no decimals.
+    """
+    return '{:,.0f}'.format(x) #this is the new syntax for formatting
+
+def thousands_format(x, pos):
+    """
+    Args are the value and tick position. 
+    Returns number of thousands with one decimal, and K in lieu of 3 zeros.
+    """
+    return '{:.0f}{}'.format(x * 1e-3, 'K') #old syntax: '%1.0fK' % (x * 1e-3)
+
+def millions_format(x, pos):
+    """
+    Args are the value and tick position. 
+    Returns number of millions with one decimal, and M in lieu of 6 zeros.
+    """
+    return '{:.1f}{}'.format(x * 1e-6, 'M')
+
+def millions_currency(x, pos):
+    """
+    Args are the value and tick position. 
+    Returns number of millions with a $ sign, M in lieu of 6 zeros, and no decimals.
+    """
+    return '{}{:.0f}{}'.format('$', x * 1e-6, 'M')
+
+#Define robust function to automatically add annotated labels on bar plots.
+#Inspiration from http://composition.al/blog/2015/11/29/a-better-way-to-add-labels-to-bar-charts-with-matplotlib/
+
+def annotate_labels(ax, labels_list, **kwargs):
+    """
+    Function to automatically add labels on bar charts.
+    It takes a plot axis, an ordered list of labels, and text kwargs.
+    """
+    
+    # Get y-axis height to calculate label position from.
+    (y_bottom, y_top) = ax.get_ylim()
+    y_height = y_top - y_bottom
+    
+    rects = ax.patches
+
+    for rect, label in zip(rects, labels_list):
+        height = rect.get_height()
+        p_height = (height / y_height) # Fraction of axis height taken up by this rectangle
+        label_position = height + (y_height * 0.01)
+        
+        # If we can fit the label above the column, do that;
+        # otherwise, put it inside the column (or else, change ylim)
+        #if p_height > 0.95:
+        #    label_position = height - (y_height * 0.05)
+        #else:
+        #    label_position = height + (y_height * 0.01)
+
+        ax.text(rect.get_x() + rect.get_width()/2., label_position, label, kwargs)
+    return None
+
+#Create a plotting dataframe with counts (number of trips) grouped by day:
+byDate = df_viz.groupby('pu_date')['id'].count() #365 complete entries
+
+fig = plt.figure()
+
+ax = byDate.plot(figsize = (16, 8), fontsize = 12, ylim = (10000, 170000), color = 'navy')
+
+formatter = FuncFormatter(thousands_format)
+ax.yaxis.set_major_formatter(formatter)
+
+
+plt.title('Total Trips per Day with Annotation of Some Major Events and Holidays', fontsize= 20, color='navy')
+plt.tick_params(labelsize=14)
+plt.xlabel('')
+
+weekday_labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+month_labels = ['Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
+
+# Create plots comparing demand per different time categories:
+fig = plt.figure(figsize = (15,16))
+graph_div = py.offline.plot(fig, auto_open = False, output_type="div")
+formatter = FuncFormatter(millions_format)
+
+plt.subplot(3,1,1) #this plot could have a polar format, although I'm not sure it'd be a better viz.
+ax1 = sns.countplot(df_viz.hour, order = range(24))
+ax1.yaxis.set_major_formatter(formatter)
+plt.tick_params(labelsize=13)
+plt.title('Total Number of Trips Between 9/1/2014 and 8/31/2015', fontsize=17, color='navy')
+plt.xlabel('Hour', fontsize=13)
+
+plt.subplot(3,1,2)
+ax2 = sns.countplot(df_viz.weekday, order = range(7))
+ax2.set_xticklabels(weekday_labels)
+ax2.yaxis.set_major_formatter(formatter)
+plt.tick_params(labelsize=13)
+plt.xlabel('')
+
+plt.subplot(3,1,3) #same as the line plot but with less granularity, perhaps easier to visualize the MOM growth
+ax3 = sns.countplot(df_viz.month)
+ax3.set_xticklabels(month_labels)
+ax3.yaxis.set_major_formatter(formatter)
+#plt.text(0, 3100000, 'Consistent growth in the number of trips from month to month!', color='navy', size=15)
+plt.tick_params(labelsize=13)
+plt.xlabel('')
+
+#fig.subplots_adjust(hspace=.5)
+fig2 = plt.show()
+
+#Create dataframe with grouped revenue data:
+byDateRev = df_viz.groupby('month')['est_revenue'].sum()
+
+#Estimate Uber's gross margin for the entire 1 year period based on 25% share of the base revenue.
+sumRev = byDateRev.sum()
+
+#Create chart showing total base revenue per month.
+months_seq = [9,10,11,12,1,2,3,4,5,6,7,8]
+formatter = FuncFormatter(millions_currency)
+
+plt.figure(figsize = (14, 7))
+ax = sns.barplot(byDateRev.index, byDateRev.values, order = months_seq)
+ax.set_xticklabels(month_labels)
+ax.yaxis.set_major_formatter(formatter)
+
+font = {'color': 'navy', 'size': 15} #text parameters
+plt.text(0, 55000000, 'Total Base Revenue for the Period: ${:,.0f} Million'.format(sumRev * 1e-6), 
+         fontdict = font) #params: x, y, text, fontdict
+plt.xlabel('')
+plt.ylabel('Revenue (Millions)', fontsize = 15)
+plt.tick_params(labelsize=13)
+plt.title('Estimated Monthly Base Revenue from Sep-2014 to Aug-2015', fontsize = 17, color='navy')
+
+fig3 = plt.show()
+
+#Create dictionary with calculated month over month revenue percentage growth:
+dict_growth = {}
+for i in range(len(months_seq)):
+    mo = months_seq[i]
+    rev = byDateRev[mo]
+    if mo == 9:
+        growth_pct = 0
+    elif mo == 1:
+        growth_pct = ((rev/byDateRev[12]) - 1) * 100
+    else:
+        growth_pct = ((rev/byDateRev[mo - 1]) - 1) * 100
+    print (month_labels[i], 'Revenue =', '${:,.0f}'.format(rev), 'Growth % = ', '{:.1f}'.format(growth_pct))
+    dict_growth[month_labels[i]] = growth_pct
+
+#Annotate Total Cummulative Growth for the Period
+
+df_plt1 = pd.DataFrame(pd.Series(dict_growth), index = month_labels, columns = ['growth_pct'])
+
+labels_list = df_plt1.growth_pct.values.round(1)
+kwargs = {'fontsize': 12, 'ha':'center', 'va': 'bottom', 'weight': 'bold', 'color': 'navy'}
+
+ax = df_plt1.plot(kind = 'bar', figsize = (15,7), fontsize = 14, rot = 0, legend=False)#df_plt2[1:] to exclude Sep
+
+ax.get_yaxis().set_ticks([])
+annotate_labels(ax, labels_list, **kwargs)
+
+plt.axhline(0, color='yellow')
+plt.title('Month Over Month Percentage Growth of Base Revenue From September 2014', 
+          fontsize = 17, color='navy')
+plt.ylabel('Growth %', fontsize = 15, weight='bold', color='navy')
+plt.ylim(-2, 22)
+plt.tick_params(labelsize=15)
+
+fig4 = plt.show()
+
+#Create dataframe with grouped data.
+df_plt2 = df_viz.groupby('weekday')['trip_mph_avg'].median() #used median since distribution is skewed.
+
+#Create plot. Does Saturday have better traffic flow than Monday?
+
+fig = plt.figure()
+labels_list = df_plt2.values.round(1)
+kwargs = {'fontsize': 12, 'ha':'center', 'va': 'bottom', 'weight': 'bold', 'color': 'navy'}
+
+ax = df_plt2.plot(kind='bar', rot = 0, figsize = (12, 6), fontsize = 12, edgecolor='yellow', linewidth=1)
+ax.set_xticklabels(weekday_labels, weight='bold')
+
+ax.get_yaxis().set_ticks([])
+annotate_labels(ax, labels_list, **kwargs)
+
+plt.xlabel('')
+plt.ylabel('Median Trip Speed (mph)', fontsize = 14)
+plt.tick_params(labelsize=14)
+plt.title('Which day of the week has the worst traffic?', fontsize = 18, color='navy')
+fig5 = plt.show()
+
+#Create plot similar to the above but with the count of trips per hour, comparing weekday vs weekend and 
+#distances >=5 versus < 5 miles. Indeed, the plots show that there is a higher demand for trips that go beyond 
+#miles (outside Manhattan) from 5 to 7 AM on weekdays, and from 6 to 8 AM on weekends (Saturday and Sunday).
+
+fig = plt.figure(figsize = (16,12))
+formatter = FuncFormatter(thousands_format)
+
+
+plt.subplot(2,2,1)
+ax1 = df_viz[(df_viz.weekday < 5) & (df_viz.trip_distance >= 5)].\
+groupby('hour')['trip_distance'].count().plot(kind='bar', rot = 0)
+ax1.yaxis.set_major_formatter(formatter)
+ellipse = Ellipse(xy=(5, 150000), width=4, height=200000, edgecolor='red', fc='None', lw=1.5)
+ax1.add_patch(ellipse)
+plt.xlabel('Hour', fontsize=14, weight='bold', color='navy')
+plt.ylabel('Demand (number of trips)', fontsize=14, weight='bold', color='navy')
+plt.ylim(0, 1400000)
+plt.title('Weekday, 5 miles or more', fontsize=14, weight='bold', color='navy')
+
+plt.subplot(2,2,2)
+ax2 = df_viz[(df_viz.weekday < 5) & (df_viz.trip_distance < 5)].\
+groupby('hour')['trip_distance'].count().plot(kind='bar', rot = 0)
+ax2.yaxis.set_major_formatter(formatter)
+ellipse = Ellipse(xy=(5, 150000), width=4, height=200000, edgecolor='red', fc='None', lw=1.5)
+ax2.add_patch(ellipse)
+plt.xlabel('Hour', fontsize=14, weight='bold', color='navy')
+plt.title('Weekday, less than 5 miles', fontsize=14, weight='bold', color='navy')
+
+plt.subplot(2,2,3)
+ax3 = df_viz[(df_viz.weekday >= 5) & (df_viz.trip_distance >= 5)].\
+groupby('hour')['trip_distance'].count().plot(kind='bar', rot = 0)
+ax3.yaxis.set_major_formatter(formatter)
+ellipse = Ellipse(xy=(6.5, 50000), width=4, height=80000, edgecolor='red', fc='None', lw=1.5)
+ax3.add_patch(ellipse)
+plt.xlabel('Hour', fontsize=14, weight='bold', color='navy')
+plt.ylabel('Demand (number of trips)', fontsize=14, weight='bold', color='navy')
+plt.ylim(0, 500000)
+plt.title('Weekend, 5 miles or more', fontsize=14, weight='bold', color='navy')
+
+plt.subplot(2,2,4)
+ax4 = df_viz[(df_viz.weekday >= 5) & (df_viz.trip_distance < 5)].\
+groupby('hour')['trip_distance'].count().plot(kind='bar', rot = 0)
+ax4.yaxis.set_major_formatter(formatter)
+ellipse = Ellipse(xy=(6.5, 50000), width=4, height=80000, edgecolor='red', fc='None', lw=1.5)
+ax4.add_patch(ellipse)
+plt.xlabel('Hour', fontsize=14, weight='bold', color='navy')
+plt.title('Weekend, less than 5 miles', fontsize=14, weight='bold', color='navy')
+
+
+fig.subplots_adjust(hspace=0.4)
+fig6 = plt.show()
+
+#Define data to plot:
+df_plt3 = df_viz.pivot_table('id', aggfunc='count', index='origin_taz', columns='destination_taz')
+
+#Visualize the origin versus destination matrix via a heatmap:
+
+fig = plt.figure(figsize=(9,9))
+
+cmap = sns.cubehelix_palette(8, start=2, rot=0, dark=0, light=.9, as_cmap=True) #'YlOrRd' #ggplot:'viridis'
+
+sns.heatmap(df_plt3, cmap=cmap, linewidths=0.1, cbar=False)
+
+plt.title('Total Trips by Pickup and Drof-Off Taxi Zones', fontsize=18, color='navy')
+plt.xlabel('Destination', fontsize=15, fontname='Tahoma', color='navy')
+plt.ylabel('Origin', fontsize=15, fontname='Tahoma', color='navy')
+plt.tick_params(labelsize=11)
+fig7 = plt.show()
+
+#Median trip average speed during weekdays:
+df_plt7 = df_viz[df_viz.weekday < 5].groupby('hour')['trip_mph_avg'].median()
+
+plt.figure(figsize =(14, 6))
+kwargs = {'fontsize': 12, 'ha':'center', 'va': 'top', 'color': 'navy', 'weight': 'bold'}
+
+#weekdays only: rush hour traffic 7-9 + 16-18
+ax = df_plt7.plot(marker = 'o', color = 'navy')
+
+for x, y in zip(df_plt7.index, df_plt7.values):
+    ax.annotate('{:.0f}'.format(y), xy=(x, y), xytext= (0, 24), textcoords='offset points', **kwargs)
+
+
+ax.get_yaxis().set_ticks([]) #hide tick labels on y-axis
+plt.fill([7,9,9,7], [0,0,30,30], 'cyan', alpha=0.2)
+plt.fill([16,18,18,16], [0,0,30,30], 'cyan', alpha=0.2)
+plt.xticks(range(24))
+plt.xlabel('Hour', fontsize=14)
+plt.ylabel('Trip Average Speed', fontsize=14)
+plt.ylim(5, 30)
+plt.xlim(-0.5, 23.5)
+plt.tick_params(labelsize=14)
+plt.title('Weekday Average Speed per Hour of the Day - Highlight for Peak Traffic', fontsize = 16, color='navy')
+fig8 = plt.show()
 
 def home(request):
      return render(
@@ -42,9 +437,21 @@ def dashboard (request):
         request,
         'hello/Dashboard.html',
         {
-            'head': head
+            'data':allData,
+            'data2':allData2,
+            'day': holidays,
+            'stats':stats,
+            'tripsPerDay':graph_div,
+            'totalTrips':fig2,
+            'monthyRev':fig3,
+            'growth':fig4,
+            'tripMedianDistance':fig5,
+            'numOfTripsDemands':fig6,
+            'pick-up_map':fig7,
+            'avgSpeed':fig8
         }
-    )
+     )
+
 def aboutus(request):
     return render(
         request,
