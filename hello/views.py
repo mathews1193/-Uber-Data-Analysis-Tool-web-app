e752f9aacc82939150fc72506c02d066b641ecec
mathews1193 <mathews1193@gmail.com>
2021-12-02 09:37:18 -0600

    working batch

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,13 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from matplotlib.ticker import FuncFormatter #Call formatter function to format tick values
+from matplotlib.offsetbox import (OffsetImage, AnnotationBbox) #Create image box
+from matplotlib.patches import Ellipse #Draw ellips
+from pandas.tseries.holiday import USFederalHolidayCalendar
 import seaborn as sns
+import chart_studio.plotly as py
 import time
 import datetime
 
@@ -21,12 +27,177 @@ pd.set_option('display.float_format', lambda x: '%.4f' % x)
 #filename = 'uber_nyc_data.csv'
 #urllib.request.urlretrieve(url, filename)
 
-
 df_uber = pd.read_csv('uber_nyc_data.csv')
-infor = df_uber.info() 
 head = df_uber.head()
 
-import datetime
+allData=[]
+for i in range(head.shape[0]):
+    temp=head.iloc[i]
+    allData.append(dict(temp))
+
+def dateParser(s):
+    """
+    Function that takes a string in the format yyyy-mm-dd hh:mm:ss, and
+    returns the same as a datetime object.
+    """
+    return datetime.datetime(int(s[0:4]), int(s[5:7]), int(s[8:10]), int(s[11:13]))
+
+#Apply parser to data
+t0 = time.time()
+df_uber['pu_date_hour'] = df_uber.pickup_datetime.apply(dateParser)
+time.time() - t0
+
+#Data encompasses exactly 365 days from Sept 1st, 2014 to Sep 1st, 2015.
+beginning = df_uber.pu_date_hour.min()
+end = df_uber.pu_date_hour.max()
+
+#Drop repeated column
+df_uber = df_uber.drop('pickup_datetime', axis=1)
+
+#if return_name is True, then name is series value and date is the series index, otherwise date is series value.
+holidays = USFederalHolidayCalendar().holidays(beginning, end, return_name = True)
+
+#Extract the weekday for each holiday
+holidays.index.map(lambda x: x.strftime('%a'))
+
+#For completeness, let's add the day before and after the holidays (better: add day after if Thursday)
+holidays_all = pd.concat([holidays, "Day After " + holidays.shift(1, 'D'), "Day Before " + holidays.shift(-1, 'D')])
+holidays_all = holidays_all.sort_index()
+holidays_all.head(10)
+
+#Create a column with the date, without the time. This will be useful later.
+t0 = time.time()
+df_uber['pu_date'] = pd.Series(map(lambda x: x.astype('datetime64[D]'), df_uber['pu_date_hour'].values))
+#df_uber['pu_date'] = df_uber['pu_date_hour'].dt.date #this is way less memory effective and 11 sec slower to execute
+time.time() - t0
+
+#Get month and year from pick up timestamp
+df_uber['year'] = df_uber['pu_date_hour'].dt.year
+df_uber['month'] = df_uber['pu_date_hour'].dt.month
+
+#Get trip pick up day of the month
+t0 = time.time()
+df_uber['day'] = df_uber['pu_date_hour'].dt.day #.apply(lambda x: x.day) = 105 seconds vs < 2 seconds with dt
+time.time() - t0
+
+#Get trip pick up day of the month
+t0 = time.time()
+df_uber['day'] = df_uber['pu_date_hour'].dt.day #.apply(lambda x: x.day) = 105 seconds vs < 2 seconds with dt
+time.time() - t0
+
+#Get trip pick up hour from timestamp
+t0 = time.time()
+df_uber['hour'] = df_uber['pu_date_hour'].dt.hour #< 2 seconds vs 101 seconds using apply lambda
+time.time() - t0
+
+head2 = df_uber.head()
+
+allData2=[]
+for i in range(head2.shape[0]):
+    temp=head2.iloc[i]
+    allData2.append(dict(temp))
+
+def duration_to_minutes(s):
+    """
+    Function that takes a string with the hh:mm:ss format and
+    returns the integer equivalent of the total time in minutes, 
+    or zero for missing values in a Pandas dataframe.
+    """
+    if pd.isnull(s):
+        val = 0 #note: this fills with 0 the 38 instances with null (missing) values
+    else:
+        hms = s.split(':')
+        val = int(hms[0])*60 + int(hms[1]) + int(hms[2])/60.0
+    return val
+
+#Transform the trip duration string into trip duration in minutes:
+t0 = time.time()
+df_uber['duration_min'] = df_uber.trip_duration.apply(duration_to_minutes) 
+time.time() - t0
+
+#Calculate average trip speed (mph) to help in understanding outliers (like trips with duration >10 h):
+df_uber['trip_mph_avg'] = df_uber.trip_distance/(df_uber.duration_min/60.0)
+
+#Drop redundant trip_duration columns
+df_uber = df_uber.drop('trip_duration', axis=1)
+df_uber = df_uber.drop('pu_date_hour', axis=1)
+
+#Source: http://uberestimate.com/prices/New-York-City/ for Uber X
+base_fare = 2.55
+per_minute = 0.35
+per_mile = 1.75
+min_fare = 8
+
+#Using eval() is even more efficient for large datasets than resorting to Numpy! 
+#Local variables must be followed by @. Only for arithmetic, cannot use if statement, for example.
+df_uber['est_revenue'] = df_uber.eval('@base_fare + duration_min * @per_minute + trip_distance * @per_mile')
+df_uber.loc[df_uber.est_revenue < 8, 'est_revenue'] = min_fare
+
+#Create dataframe to be used for visualization with exactly 365 days of data, and max trip duration of 16h:
+df_viz = df_uber[(df_uber.pu_date != datetime.datetime(2015, 9, 1)) & (df_uber.duration_min <= 960)].copy() #1975 cases
+
+#Descriptive statistics for numerical features:
+stats = df_viz[['duration_min', 'trip_distance', 'trip_mph_avg', 'est_revenue']].describe()
+
+#Create functions to format tick numbers
+def thousands_comma(x, pos):
+    """
+    Args are the value and tick position. 
+    Returns number with thousands comma and no decimals.
+    """
+    return '{:,.0f}'.format(x) #this is the new syntax for formatting
+
+def thousands_format(x, pos):
+    """
+    Args are the value and tick position. 
+    Returns number of thousands with one decimal, and K in lieu of 3 zeros.
+    """
+    return '{:.0f}{}'.format(x * 1e-3, 'K') #old syntax: '%1.0fK' % (x * 1e-3)
+
+def millions_format(x, pos):
+    """
+    Args are the value and tick position. 
+    Returns number of millions with one decimal, and M in lieu of 6 zeros.
+    """
+    return '{:.1f}{}'.format(x * 1e-6, 'M')
+
+def millions_currency(x, pos):
+    """
+    Args are the value and tick position. 
+    Returns number of millions with a $ sign, M in lieu of 6 zeros, and no decimals.
+    """
+    return '{}{:.0f}{}'.format('$', x * 1e-6, 'M')
+
+#Define robust function to automatically add annotated labels on bar plots.
+#Inspiration from http://composition.al/blog/2015/11/29/a-better-way-to-add-labels-to-bar-charts-with-matplotlib/
+
+def annotate_labels(ax, labels_list, **kwargs):
+    """
+    Function to automatically add labels on bar charts.
+    It takes a plot axis, an ordered list of labels, and text kwargs.
+    """
+    
+    # Get y-axis height to calculate label position from.
+    (y_bottom, y_top) = ax.get_ylim()
+    y_height = y_top - y_bottom
+    
+    rects = ax.patches
+
+    for rect, label in zip(rects, labels_list):
+        height = rect.get_height()
+        p_height = (height / y_height) # Fraction of axis height taken up by this rectangle
+        label_position = height + (y_height * 0.01)
+        
+        # If we can fit the label above the column, do that;
+        # otherwise, put it inside the column (or else, change ylim)
+        #if p_height > 0.95:
+        #    label_position = height - (y_height * 0.05)
+        #else:
+        #    label_position = height + (y_height * 0.01)
+
+        ax.text(rect.get_x() + rect.get_width()/2., label_position, label, kwargs)
+    return None
+
 
 def home(request):
      return render(
@@ -42,9 +213,13 @@ def dashboard (request):
         request,
         'hello/Dashboard.html',
         {
-            'head': head
+            'data':allData,
+            'data2':allData2,
+            'day': holidays,
+            'stats':stats
         }
-    )
+     )
+
 def aboutus(request):
     return render(
         request,
